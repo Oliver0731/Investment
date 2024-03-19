5ada9700893ae986bc4e8df3b2028773197456d1
Oliver0731 <o.townell@share.epsb.ca>
2024-03-19 09:30:04 -0600

    update main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,12 @@
 let outputEl = document.getElementById("output");
 
 // Global Variables
-let data = [2000, 4000, 3000, 4500, 1500]; // account data
+let data = [];
+for (let n = 0; n < 50; n++) {
+  data.push(randomInt(0, 5000));
+}
+// account data
 let maxVal = 5000; // max data value
-
 // Draw Array every 20ms
 setInterval(drawDataArray, 20);
 
@@ -36,6 +39,10 @@ function mainMenu() {
 // ******************************************************
 function deposit() {
   // Prompt the user for the index of an account and the amount to deposit into that account.
+  let account = +prompt("enter account number");
+  let amount = +prompt("enter deposite amount");
+  data.splice(account, 1, data[account] + amount);
+  data[account] += amount;
   // Modify the data array to reflect the deposit.
   // Adjust the maxVal variable if necessary.
   // Use the outputEl to provide a confirmation message.
@@ -45,6 +52,10 @@ function deposit() {
 
 function withdrawal() {
   // Prompt the user for the index of an account and the amount to withdraw from that account.
+  let account = +prompt("enter account number");
+  let amount = +prompt("enter withdraw amount");
+  data.splice(account, 1, data[account] - amount);
+  data[account] -= amount;
   // Modify the data array to reflect the withdrawal.
   // Check to assure that the account has enough funds.
   // Use the outputEl to provide a confirmation message.
@@ -53,10 +64,15 @@ function withdrawal() {
 }
 
 function countUnder2000() {
+  let count = 0;
   // Count the number of accounts that are less than 2000
-  // Use the outputEl to display the results of the count.
-
-  outputEl.innerHTML = "Count Under $2000";
+  // Use the outputEl to display the results of the count
+  for (let n = 0; n <= 50; n++) {
+    if (data[n] < 2000) {
+      count++;
+    }
+  }
+  outputEl.innerHTML = `accounts under 2000: ${count}`;
 }
 
 function generousDonor() {
@@ -64,12 +80,19 @@ function generousDonor() {
   // account that has less than $2000.
   // Modify the data array to apply this donation.
   // Use the outputEl to display the total amount of money that was donated.
-
+  for (let n = 0; n <= 50; n++) {
+    if (data[n] < 2000) {
+      data[n] += 500;
+    }
+  }
   outputEl.innerHTML = "Generous Donor";
 }
 
 function hackerAttack() {
   // A hacker steals 5% from every account.
+  for (let n = 0; n <= 50; n++) {
+    data[n] -= data[n] * 0.05;
+  }
   // Modify the data array to apply this theft.
   // Use the outputEl to display the total amount that was stolen.
 
